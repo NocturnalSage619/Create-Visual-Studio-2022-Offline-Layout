54b45588725d2c64c4b17d47f86d611decbc24dc
NocturnalSage619 <154988764+NocturnalSage619@users.noreply.github.com>
2024-01-24 19:56:21 -0300

    Add files via upload

---
+++ create_layout_professional_en.bat
@@ -0,0 +1,10 @@
+@echo off
+set installer=vs_professional.exe
+set layoutDir=C:\vs2022
+set lang=en-US
+
+echo Creating offline installation layout...
+
+%installer% --layout %layoutDir% --lang %lang%
+
+echo Offline installation layout created successfully.
